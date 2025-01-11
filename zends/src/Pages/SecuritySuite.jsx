ac45ccdddac5c09889ee713b0803d152b8f9988f
Daniel <hd2242kin20@gmail.com>
2025-01-11 18:22:26 -0300

    313

--- zends/src/Pages/SecuritySuite.jsx
+++ zends/src/Pages/SecuritySuite.jsx
@@ -36,52 +36,59 @@ const SS = () => {
         {/* Cards Section */}
         <div id="produtos" className="container mx-auto px-4 py-8">
     <h2 className="text-3xl font-bold text-center mb-8">Sobre o Security Suite</h2>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-20">
-      <div className="bg-neutral-900 p-6 h-[500px] text-center border-0 rounded-sm bg-gradient-to-br from-cyan-500 to-blue-900 transform transition-all hover:scale-105">
-        <h3 className="text-xl font-semibold mb-2">Desenvolvimento de Software em Segurança da Informação</h3>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mx-20">
+      <div className="bg-neutral-900 p-6 h-full text-center border-0 rounded-sm bg-gradient-to-br from-cyan-500 to-blue-900 transform transition-all hover:scale-105">
+        <h3 className="text-xl font-semibold mb-2">USB - Universal Serial Bus</h3>
         <ul className="list-disc pl-5 text-left">
           <li>Identificação de falhas, necessidades ou vulnerabilidades em sistemas (novos ou existentes).</li>
           <li>Procuramos a melhor solução utilizando conhecimento especializado na criação de APIs de criptografia sob medida.</li>
           <li>Uso de algoritmos de criptografia baseados em padrões de mercado, como 3DES/AES ou certificados digitais.</li>
           <li>Adição de uma camada de proteção para informações sigilosas, tanto para sistemas internos quanto externos.</li>
-          <li>Integração segura entre departamentos e empresas, garantindo a proteção dos dados.</li>
+          <li>Registro detalhado de operações (log)</li>
         </ul>
       </div>
   
-      <div className="bg-neutral-900 p-6 h-[500px] text-center border-0 rounded-sm bg-gradient-to-br from-cyan-500 to-blue-900 flex flex-col justify-between transform transition-all hover:scale-105">
-        <div>
-          <h3 className="text-2xl font-semibold mb-2">Zends Security Suite</h3>
-          <p className="text-lg">Software de segurança para controle e monitoração de dispositivos USB ou seriais, arquivos, pastas, processos e comunicação.
-  Solução baseada em Drivers de Kernel da plataforma Windows, permite bloquear qualquer acesso não autorizado ao sistema.
-  Zends Security Suite estende sua tecnologia através de interfaces de programação (APIs) para utilização pelos sistemas e aplicativos de nossos clientes, blindando-os contra ataques inadivertidos ou intencionais.</p>
-        </div>
-        {/* Centraliza o botão "Veja Mais" no final do card */}
-        <a 
-  href="/securitysuite" 
-  className="w-56 h-12 px-6 py-3 text-white border rounded-sm border-white font-semibold hover:border-cyan-500 hover:bg-gradient-to-br hover:tracking-wide hover:from-cyan-500 hover:to-blue-900 hover:text-white flex items-center justify-center gap-2 transition-all drop-shadow-2xl self-center mt-auto"
-  onClick={(e) => {
-    e.preventDefault();
-    // Navegar para a página de Security Suite
-    window.location.href = '/securitysuite'; // Redireciona diretamente para a página de Security Suite
-  }}
->
-  Veja Mais
-</a>
+      
+  
+      <div className="bg-neutral-900 p-6 h-full text-center border-0 rounded-sm bg-gradient-to-br from-cyan-500 to-blue-900 transform transition-all hover:scale-105">
+        <h3 className="text-xl font-semibold mb-2">Processos</h3>
+        <ul className="list-disc pl-5 text-left">
+    <p className="font-semibold">Whitelist:</p>
+    <li>Apenas programas cadastrados podem ser executados</li>
+    <li>Formato PE (exe, dll, sys, ocx)</li>
+    <p className="font-semibold">Blacklist:</p>
+    <li>Exclusão automática de programa cadatrado como nocivo ou indesejado</li>
+    <li>Detecção de criação de executável (PE)</li>
+    <li>Registro detalhado de operações (log)</li>
+   </ul>
 
       </div>
-  
-      <div className="bg-neutral-900 p-6 h-[500px] text-center border-0 rounded-sm bg-gradient-to-br from-cyan-500 to-blue-900 transform transition-all hover:scale-105">
-        <h3 className="text-xl font-semibold mb-2">Desenvolvimento de Software em TI</h3>
+
+      <div className="bg-neutral-900 p-6 h-full text-center border-0 rounded-sm bg-gradient-to-br from-cyan-500 to-blue-900 transform transition-all hover:scale-105">
+        <h3 className="text-xl font-semibold mb-2">SAFEBOX</h3>
         <ul className="list-disc pl-5 text-left">
-    <li>Desenvolvimento de Drivers de Kernel.</li>
-    <li>Desenvolvimento de Serviços do Windows.</li>
-    <li>Criação de Servidores/Clientes de comunicação.</li>
-    <li>Integração com sistemas novos ou legados através de WebServices, TCP ou RCP, independente da linguagem em que foram desenvolvidos.</li>
-    <li>Integração de sistemas internos e externos, utilizando as principais tecnologias para garantir maior agilidade e flexibilidade no desenvolvimento.</li>
-    <li>Foco em garantir a segurança e robustez da solução, incentivando boas práticas.</li>
-  </ul>
-  
+    <li>Arquivos importantes podem ser copiados (dlls, drivers, logs, .configs e chaves do registro do Windows).</li>
+    <p className="font-semibold">Proteção de Recursos</p>
+    <li>Arquivos pastas e chaves do Windows podem ser protegidos contra exclusão, cópia, gravação e execução não autorizada.</li>
+    <li>Recursos protegidos são visíveis a todos, mas somente podem ser acessados por "Processos Autorizados", nem mesmo a operação de cópia é permitida</li>
+   <p className="font-semibold">Processos Autorizados</p>
+   <li>Programas registrados com permissão de acesso aos recursos protegidos</li>
+   </ul>
+
       </div>
+
+      <div className="bg-neutral-900 p-6 h-full text-center border-0 rounded-sm bg-gradient-to-br from-cyan-500 to-blue-900 transform transition-all hover:scale-105">
+        <h3 className="text-xl font-semibold mb-2">Comandos de Segurança e Instalação de Softwares</h3>
+        <ul className="list-disc pl-5 text-left">
+    <li>Shudown Forçado</li>
+    <li>Lockdown (tela azul simulada)</li>
+    <li>Desabilitar / Reabilitar proteções</li>
+    <li>Alerta de instalação de software com possibilidade de bloqueio mediante configuração.</li>
+    <li>Registro detalhado de operações (log)</li>
+   </ul>
+
+      </div>
+
     </div>
   </div>
   
